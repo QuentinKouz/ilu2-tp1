c4bb9a5cbb1f9b11908503a63c5e6e814b9b70c4
Quentin Kouzmitch <quentinvegeto@gmail.com>
2023-04-06 22:51:39 +0200

    Fini

--- src/histoire/ScenarioCasDegrade.java
+++ src/histoire/ScenarioCasDegrade.java
@@ -13,21 +13,21 @@ public class ScenarioCasDegrade {
 		etal.libererEtal();
 		
 		//etal.acheterProduit(1, null);
-		
+		System.out.println("test : AcheterProduit avec un nombre négatif");
 		try {
 			etal.acheterProduit(-1, obelix);
 
 		}
 		catch(IllegalArgumentException e) {
-			System.out.println("--- La quantité de produit à acheter n'est pas positive. ---");
+			System.out.println("La quantité de produit à acheter n'est pas positive.");
 		}
 		
-		
+		System.out.println("test : AcheterProduit avec un étal non occupé");
 		try {
 			etal.acheterProduit(1, obelix);
 		}
 		catch(IllegalStateException e) {
-			System.out.println("--- L'étal n'est pas occupé. ---");
+			System.out.println("L'étal n'est pas occupé.");
 		}
 		
 		System.out.println("Fin du test");
